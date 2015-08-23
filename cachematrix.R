f7f50662b9b5c4347ae62f743261a1a8a5684f99
pranavlim88 <pranavlimaye@hotmail.com>
2015-08-22 22:34:28 -0400

    pranavlim88 Assignment 2
    This is the solution to Assignment problem 2 for R programming.

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## MakeCacheMatrix will create a list of functions to:
+##1. Set the value of the matrix.
+##2. Get the value of the matrix.
+##3. Set the valve of the matrix inverse.
+##4. Get the value of the matrix inverse.
+
 
 makeCacheMatrix <- function(x = matrix()) {
+        inver <- NULL
+        set <- function(y) {
+                x <<- y
+                inver <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inver <<- inverse
+        getinverse <- function() inver
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 
 }
 
@@ -11,5 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inver <- x$getinverse()
+	  if(!is.null(inver)){
+		message ("getting cached data")
+		return (inver)
+	  }
+	data <- x$(get)
+	inver <- inverse(data, ...)
+	x$getinverse(inver)
+	inver
 }
